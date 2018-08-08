a3e0cea038f76bfa2ccbab76b62f38545f1c6ea1
Weverson Luiz <weverson408@gmail.com>
2018-08-08 01:09:43 -0300

    Criando operação de instanciação

--- src/main/java/br/gov/go/cursomc/CursomcApplication.java
+++ src/main/java/br/gov/go/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package br.gov.go.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.gov.go.cursomc.domain.Categoria;
+import br.gov.go.cursomc.repositories.CategoriaRepository;
 
 @SpringBootApplication
 public class CursomcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(CategoriaRepository categoriaRepository){
+		return args -> {
+			initCategorias(categoriaRepository);
+		};
+	}
+	
+	private void initCategorias(CategoriaRepository categoriaRepository){
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Eletrônicos");
+		Categoria cat4 = new Categoria(null, "Cama, Mesa e Banho");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		
 	}
 }
