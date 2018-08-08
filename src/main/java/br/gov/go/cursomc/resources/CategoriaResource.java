f24d63cf78537c033a2374fc34ad364371996702
Weverson Luiz <weverson408@gmail.com>
2018-08-07 21:03:35 -0300

    Testando minha primeira classe de domínio

--- src/main/java/br/gov/go/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/gov/go/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.gov.go.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.go.cursomc.domain.Categoria;
+
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Rest esta funcionando";
+	public List<Categoria> listar(){
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 
 }
