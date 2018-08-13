bb388a62321c6030618ca639860bf0323aabf489
weverson <weverson@weverson-nb>
2018-08-12 22:18:51 -0300

    Validação sintática com BeanValidation

--- src/main/java/br/gov/go/cursomc/dto/CategoriaDTO.java
+++ src/main/java/br/gov/go/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package br.gov.go.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.gov.go.cursomc.domain.Categoria;
 
 public class CategoriaDTO  implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	@NotEmpty(message="O preenchimento do campo nome é obrigatório.")
+	@Length(min=5, max=80, message="O campo nome deve ter entre 5 e 80 caracteres.")
 	private String nome;
 	
 	public CategoriaDTO(){}
--- src/main/java/br/gov/go/cursomc/services/CategoriaService.java
+++ src/main/java/br/gov/go/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.gov.go.cursomc.domain.Categoria;
+import br.gov.go.cursomc.dto.CategoriaDTO;
 import br.gov.go.cursomc.repositories.CategoriaRepository;
 import br.gov.go.cursomc.services.Exceptions.DataIntegrityException;
 import br.gov.go.cursomc.services.Exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto){
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
